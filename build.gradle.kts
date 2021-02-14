fdd7f66a6af08bfcdb4b7234ee8ec10108335cf9
Taoning Ge <tony.bazhang@gmail.com>
2021-02-14 19:11:57 +0800

    Update `mirai-core` and `mirai-console` to the latest(2.3.2).

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.0-RC" // mirai-console version
+    id("net.mamoe.mirai-console") version "2.3.2" // mirai-console version
 }
 
 mirai {
-    coreVersion = "2.0-RC" // mirai-core version
+    coreVersion = "2.3.2" // mirai-core version
 }
 
 group = "org.example"
