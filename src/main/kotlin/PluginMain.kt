6a508063caa1b0a36a5bd6da99ef7981c7781058
Him188 <Him188@mamoe.net>
2021-03-01 08:37:32 +0800

    Add author and info

--- src/main/kotlin/PluginMain.kt
+++ src/main/kotlin/PluginMain.kt
@@ -9,7 +9,16 @@ object PluginMain : KotlinPlugin(
         id = "org.example.mirai-plugin",
         name = "ExamplePlugin",
         version = "0.1.0"
-    )
+    ) {
+        author("作者名称或联系方式")
+
+        info("""
+            这是一个测试插件, 
+            在这里描述插件的功能和用法等.
+        """.trimIndent())
+
+        // author 和 info 可以删除.
+    }
 ) {
     override fun onEnable() {
         logger.info { "Plugin loaded" }
