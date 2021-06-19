bac512f4b510e3082f5a94703ff74a5d90d3fc30
Eritque arcus <1930893235@qq.com>
2021-06-19 16:34:34 +0800

    Update src/main/kotlin/PluginMain.kt
    Co-authored-by: Karlatemp <karlatemp@vip.qq.com>

--- src/main/kotlin/PluginMain.kt
+++ src/main/kotlin/PluginMain.kt
@@ -12,16 +12,19 @@ import net.mamoe.mirai.message.data.Image.Key.queryUrl
 import net.mamoe.mirai.message.data.PlainText
 import net.mamoe.mirai.utils.info
 
-/*
-使用kotlin版请把
-[src/main/resources/META-INF.services/net.mamoe.mirai.console.plugin.jvm.JvmPlugin]
-文件内容改成[org.example.mirai.plugin.PluginMain]也就是当前主类
-使用kt可以把java文件夹删除不会对项目有影响
-
-在settings.gradle.kts里改生成的插件mirai.jar名称
-build.gradle.kts里改依赖库和插件版本
-在主类下的JvmPluginDescription改插件名称，id和版本
-用runmiraikt这个配置可以在ide里运行，不用复制到mcl或其他启动器
+/**
+ * 使用 kotlin 版请把
+ * `src/main/resources/META-INF.services/net.mamoe.mirai.console.plugin.jvm.JvmPlugin`
+ * 文件内容改成 `org.example.mirai.plugin.PluginMain` 也就是当前主类全类名
+ *
+ * 使用 kotlin 可以把 java 源集删除不会对项目有影响
+ *
+ * 在 `settings.gradle.kts` 里改构建的插件名称、依赖库和插件版本
+ *
+ * 在该示例下的 [JvmPluginDescription] 修改插件名称，id和版本，etc
+ *
+ * 可以使用 `src/test/kotlin/RunMirai.kt` 在 ide 里直接调试，
+ * 不用复制到 mirai-console-loader 或其他启动器中调试
  */
 
 object PluginMain : KotlinPlugin(
