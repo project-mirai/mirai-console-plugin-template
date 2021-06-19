483adc687ad8bd8c6a5a443528b09a40ebedd8b4
Eritque arcus <1930893235@qq.com>
2021-06-19 17:23:16 +0800

    语言名称大写

--- src/test/kotlin/RunMirai.kt
+++ src/test/kotlin/RunMirai.kt
@@ -9,10 +9,10 @@ import net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader
 suspend fun main() {
     MiraiConsoleTerminalLoader.startAsDaemon()
 
-    //如果是kotlin
+    //如果是Kotlin
     PluginMain.load()
     PluginMain.enable()
-    //如果是java
+    //如果是Java
 //    JavaPluginMain.INSTANCE.load()
 //    JavaPluginMain.INSTANCE.enable()
 
