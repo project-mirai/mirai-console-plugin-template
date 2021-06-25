e576d688d32808af52c45a4b00d19ada69c4646a
Eritque arcus <1930893235@qq.com>
2021-06-25 18:02:13 +0800

    更新java版本，增加注释，更新mirai版本 (#8)
    * 更新java版本，增加注释，更新mirai版本
    
    * Update src/main/java/org/example/mirai/plugin/JavaPluginMain.java
    
    Co-authored-by: Karlatemp <karlatemp@vip.qq.com>
    
    * Update src/main/kotlin/PluginMain.kt
    
    Co-authored-by: Karlatemp <karlatemp@vip.qq.com>
    
    * 回退kt版本，更新EventChannel
    
    * Update src/main/java/org/example/mirai/plugin/JavaPluginMain.java
    
    Co-authored-by: Him188 <Him188@mamoe.net>
    
    * Update src/main/java/org/example/mirai/plugin/JavaPluginMain.java
    
    Co-authored-by: Him188 <Him188@mamoe.net>
    
    * Update src/main/java/org/example/mirai/plugin/JavaPluginMain.java
    
    Co-authored-by: Him188 <Him188@mamoe.net>
    
    * Update src/main/java/org/example/mirai/plugin/JavaPluginMain.java
    
    Co-authored-by: Him188 <Him188@mamoe.net>
    
    * 语言名称大写
    
    Co-authored-by: Karlatemp <karlatemp@vip.qq.com>
    Co-authored-by: Him188 <Him188@mamoe.net>

--- src/test/kotlin/RunMirai.kt
+++ src/test/kotlin/RunMirai.kt
@@ -9,8 +9,12 @@ import net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader
 suspend fun main() {
     MiraiConsoleTerminalLoader.startAsDaemon()
 
+    //如果是Kotlin
     PluginMain.load()
     PluginMain.enable()
+    //如果是Java
+//    JavaPluginMain.INSTANCE.load()
+//    JavaPluginMain.INSTANCE.enable()
 
     val bot = MiraiConsole.addBot(123456, "") {
         fileBasedDeviceInfo()
