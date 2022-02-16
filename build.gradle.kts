28304da756ffce88b24b39766c1249d74d580eb0
Him188 <Him188@mamoe.net>
2022-02-16 14:20:49 +0000

    Use mirai 2.10.0 with Kotlin 1.6.10, remove redundant repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,15 @@
 plugins {
-    val kotlinVersion = "1.4.30"
+    val kotlinVersion = "1.6.10"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.6.6"
+    id("net.mamoe.mirai-console") version "2.10.0"
 }
 
 group = "org.example"
 version = "0.1.0"
 
 repositories {
-    mavenLocal()
     maven("https://maven.aliyun.com/repository/public") // 阿里云国内代理仓库
     mavenCentral()
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,1 @@
-pluginManagement {
-    repositories {
-        mavenLocal()
-        gradlePluginPortal()
-        mavenCentral()
-        maven("https://dl.bintray.com/kotlin/kotlin-eap")
-    }
-}
 rootProject.name = "mirai-console-plugin-template"
