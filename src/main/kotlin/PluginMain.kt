0365f0d6506e413ae41d00713b5820c15a598ea4
Karlatemp <kar@kasukusakura.com>
2022-12-05 23:51:59 +0800

    mirai-console permissions example; reformat

--- src/main/kotlin/PluginMain.kt
+++ src/main/kotlin/PluginMain.kt
@@ -1,14 +1,17 @@
 package org.example.mirai.plugin
 
+import net.mamoe.mirai.console.permission.AbstractPermitteeId
+import net.mamoe.mirai.console.permission.PermissionService
+import net.mamoe.mirai.console.permission.PermissionService.Companion.hasPermission
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
-import net.mamoe.mirai.event.EventChannel
+import net.mamoe.mirai.contact.Member
+import net.mamoe.mirai.contact.User
 import net.mamoe.mirai.event.GlobalEventChannel
 import net.mamoe.mirai.event.events.BotInvitedJoinGroupRequestEvent
 import net.mamoe.mirai.event.events.FriendMessageEvent
 import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.event.events.NewFriendRequestEvent
-import net.mamoe.mirai.event.globalEventChannel
 import net.mamoe.mirai.message.data.Image
 import net.mamoe.mirai.message.data.Image.Key.queryUrl
 import net.mamoe.mirai.message.data.PlainText
@@ -49,7 +52,7 @@ object PluginMain : KotlinPlugin(
         logger.info { "Plugin loaded" }
         //配置文件目录 "${dataFolder.absolutePath}/"
         val eventChannel = GlobalEventChannel.parentScope(this)
-        eventChannel.subscribeAlways<GroupMessageEvent>{
+        eventChannel.subscribeAlways<GroupMessageEvent> {
             //群消息
             //复读示例
             if (message.contentToString().startsWith("复读")) {
@@ -77,17 +80,39 @@ object PluginMain : KotlinPlugin(
                 }
             }
         }
-        eventChannel.subscribeAlways<FriendMessageEvent>{
+        eventChannel.subscribeAlways<FriendMessageEvent> {
             //好友信息
             sender.sendMessage("hi")
         }
-        eventChannel.subscribeAlways<NewFriendRequestEvent>{
+        eventChannel.subscribeAlways<NewFriendRequestEvent> {
             //自动同意好友申请
             accept()
         }
-        eventChannel.subscribeAlways<BotInvitedJoinGroupRequestEvent>{
+        eventChannel.subscribeAlways<BotInvitedJoinGroupRequestEvent> {
             //自动同意加群申请
             accept()
         }
+
+        myCustomPermission // 注册权限
     }
+
+    // region console 权限系统示例
+    private val myCustomPermission by lazy { // Lazy: Lazy 是必须的, console 不允许提前访问权限系统
+        // 注册一条权限节点 org.example.mirai-example:my-permission
+        // 并以 org.example.mirai-example:* 为父节点
+
+        // @param: parent: 父权限
+        //                 在 Console 内置权限系统中, 如果某人拥有父权限
+        //                 那么意味着此人也拥有该权限 (org.example.mirai-example:my-permission)
+        // @func: PermissionIdNamespace.permissionId: 根据插件 id 确定一条权限 id
+        PermissionService.INSTANCE.register(permissionId("my-permission"), "一条自定义权限", parentPermission)
+    }
+
+    public fun hasCustomPermission(sender: User): Boolean {
+        return when (sender) {
+            is Member -> AbstractPermitteeId.ExactMember(sender.group.id, sender.id)
+            else -> AbstractPermitteeId.ExactUser(sender.id)
+        }.hasPermission(myCustomPermission)
+    }
+    // endregion
 }
